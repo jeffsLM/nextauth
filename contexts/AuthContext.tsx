dd07a4f9a30d6e9155bfdf0503a6620e9d88ebec
jeffsLM <jeffes.dev@gmail.com>
2021-11-13 13:04:02 -0300

    connect with api

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SingInCredentials = {
     email: string;
@@ -16,11 +17,18 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
-export function AuthProvider({ children }:AuthProviderProps) {
+export function AuthProvider({ children }: AuthProviderProps) {
     const isAuthenticated = false;
 
     async function singIn({ email, password }: SingInCredentials) {
-        console.log({ email, password });
+        try {
+            const response = await api.post('sessions', {
+                email, password
+            })
+            console.log(response)
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     return (
